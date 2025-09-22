14c42fd57700e0f230a4aebd1688c3f3a54beb2d
Dave Ejezie <evergreendave1@gmail.com>
2025-09-22 16:27:11 +0100

    kdvlab logos bug fix

--- src/components/layout/navigation/logo.tsx
+++ src/components/layout/navigation/logo.tsx
@@ -25,13 +25,13 @@ export const Logo: React.FC<LogoProps> = ({ className, onClick }) => {
       )}
     >
       {/* Custom Logo Image */}
-      <div className="relative h-20 w-20 flex-shrink-0">
+      <div className="relative h-40 w-40 flex-shrink-0">
         <Image
-          src="/images/Logo/logo-cropped.png"
+          src="/images/Logo/Logo-cropped.png"
           alt="KDVLAB Logo"
           fill
           className="object-contain"
-          sizes="40px"
+          sizes="160px"
           priority // Since this is the main logo, load it with priority
         />
       </div>
